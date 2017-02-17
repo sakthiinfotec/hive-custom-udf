3d8121240457a70f89e686a7f9a5ce98c54af384
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2017-02-16 22:45:05 -0700

    Description annotation changes

--- src/main/java/com/sakthiinfotec/hive/custom/ToUpperCaseUDF.java
+++ src/main/java/com/sakthiinfotec/hive/custom/ToUpperCaseUDF.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.io.Text;
  * 
  * @author sakthinfotec
  */
-@Description(name = "ToUpperCase", value = "Returns upper case of a given string", extended = "SELECT toUpperCase('Hello World!');")
+@Description(name = "ToUpperCaseUDF", value = "Returns upper case of a given string", extended = "SELECT toUpperCase('Hello World!');")
 public class ToUpperCaseUDF extends UDF {
 
 	private Text result = new Text();
